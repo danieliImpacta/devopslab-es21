7c9c9c07f808544386858ea035694bc9c62863bd
danieliImpacta <82848789+danieliImpacta@users.noreply.github.com>
2021-10-19 18:13:07 -0300

    Ajustes

--- test.py
+++ test.py
@@ -1,22 +1,20 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*- 
 from app import app
-import unittest
+import unittest                                                                                                                                                                                          
 
-class Test(unittest.TestCase):
+class Test(unittest.TestCase):                                                                                                                                                                           
 
-    def setUP(self):
-        # cria uma instância da unittest, precisa do nome "setUp"
-        self.app = app.test_client()
+    def setUp(self):                                                                                                                                                                                     
+        # cria uma instância do unittest, precisa do nome "setUp"                                                                                                                                        
+        self.app = app.test_client()                                                                                                                                                                     
 
-        # envia uma requisição GET para a URL
-        self.result = self.app.get('/')
+        # envia uma requisicao GET para a URL                                                                                                                                                            
+        self.result = self.app.get('/')                                                                                                                                                                  
 
-    def test_requisicao(self):
-            # compara o status da requisição (precisa ser igual a 200)
-            self.assertEqual(self.result.satus_code, 200)
+    def test_requisicao(self):                                                                                                                                                                           
+        # compara o status da requisicao (precisa ser igual a 200)                                                                                                                                       
+        self.assertEqual(self.result.status_code, 200)                                                                                                                                                   
 
-    def test_conteudo(self):
-            # verifica o retorno do conteúdo da página
-            self.assertEqual(self.result.data.decode('utf-8'), "Hello World")    
-
-
+    def test_conteudo(self):                                                                                                                                                                             
+        # verifica o retorno do conteudo da pagina                                                                                                                                                       
+        self.assertEqual(self.result.data.decode('utf-8'), "Hello World")
